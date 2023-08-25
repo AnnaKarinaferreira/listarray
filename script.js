d9ef6ea6f92a37424209c36900cbfa75e13ef9ae
AnnaKarinaferreira <127227830+AnnaKarinaferreira@users.noreply.github.com>
2023-08-25 09:19:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let nomes = [];
+let profissoes = []
+let nome = document.getElementById("nome");
+let prof = document.getElementById("prof");
+
+function adc(){
+    nomes.push(nome.value.toUpperCase());
+    profissoes.push(prof.value.toUpperCase());
+    console.log(nomes, profissoes);
+}
+function rmv(){
+    let pos = nomes.indexOf(nome.value.toUpperCase());
+    if(pos !== -1){
+        nomes.splice(pos,1);
+        profissoes.splice(pos,1);
+    }
+    else{
+        console.log("O nome "+nome.value+" não foi encontrado!");
+    }
+    console.log(nomes, profissoes);
+}
+
+// NomeDaString.toUpperCase()  converte em maiusculo
+// NomeDaString.toLowerCase()  coverte em minusculo 
